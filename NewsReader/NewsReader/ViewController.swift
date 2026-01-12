0e1c8acab697067f88e04f3b4bed2f459dfe5c2c
Tina Skordaki <tinaskordaki@gmail.com>
2026-01-12 13:19:12 +0200

    created project

---
+++ NewsReader/NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Tina  on 12.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
